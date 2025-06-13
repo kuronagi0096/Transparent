4c25b30a9fc16ff0c828a1ebfb079a064f933e9a
kuroino0096 <20237041-hiratayuuta@hcs.ac.jp>
2025-06-13 17:34:16 +0900

    一旦ビルド可能

--- Transparent/Actor.cpp
+++ Transparent/Actor.cpp
@@ -1,2 +1,18 @@
 #include "Actor.h"
 #include "pch.h"
+
+void Actor::Update(float deltaTime)
+{
+	if (_state == Active)
+	{
+		UpdateActor(deltaTime);
+		UpdateComponents(deltaTime);
+	}
+}
+void Actor::UpdateComponents(float deltaTime)
+{
+	for (auto& component : _components)
+	{
+		component->UpdateComponent(deltaTime);
+	}
+}
